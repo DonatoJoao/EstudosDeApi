192aeb9da2e51e0c833e289a6be36465e775cade
DonatoJoao <joaod.morais@gmail.com>
2024-05-18 10:58:20 -0300

    Implementando classe Main e alguns ajustes

--- src/main/java/com/aplicacao/Main.java
+++ src/main/java/com/aplicacao/Main.java
@@ -1,2 +1,22 @@
-package com.aplicacao;public class Main {
+package com.aplicacao;
+
+import com.aplicacao.dto.EnderecoDto;
+import com.aplicacao.servico.ApiServico;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiServico apiServico = new ApiServico();
+        try {
+            EnderecoDto enderecoDto = apiServico.getEndereco("01001000");
+            System.out.println(enderecoDto.getLogradouro());
+            System.out.println(enderecoDto.getBairro());
+
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
